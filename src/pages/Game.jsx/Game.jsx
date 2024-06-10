96816ecb3d6dbb19b1529a6c6aa105f940cb5a5a
leticiaSouzaDev <lec667121@gmail.com>
2024-06-10 19:30:16 -0300

    feat: adicionando lógica ao nome do jogador

--- src/pages/Game.jsx/Game.jsx
+++ src/pages/Game.jsx/Game.jsx
@@ -5,29 +5,41 @@ import * as S from './Style.jsx'
 function Game(){
     const [nome, setNome] = useState('');
     const [jogoIniciado, setJogoIniciado] = useState(false);
+    const [mostrarMensagem, setMostrarMensagem] = useState(true);
 
     const handleNomeSubmit = (event) => { // funcão para receber o  nome do jogador
         event.preventDefault();
         setJogoIniciado(false);
 
-        console.log(nome)
+        setTimeout(() =>{
+            setMostrarMensagem(false);
+        }, 3000)
     }
 
+    document.addEventListener('keydown', function(event) { // verificando qual tecla foi precionada
+        let teclaPressionada = event.key;
+        console.log('Tecla pressionada: ' + teclaPressionada);
+    });
 
     return(
         <S.SessaoGame>
 
             <S.DivGame>
                 <h2>MINI-GAME</h2>
-                <button id="botaoIniciar" onClick={() => setJogoIniciado(true)}>Iniciar</button>
 
-                {jogoIniciado && (
+                {jogoIniciado ? (
                     <form onSubmit={handleNomeSubmit}>
                         <label>Digite seu nome</label>
                         <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}></input>
                         <button type="submit">enviar</button>
                     </form>
+                ) : (
+                    nome && mostrarMensagem &&
+                    (<p>Bem - vindo ao jogo {nome}</p>)
                 )}
+                <button id="botaoIniciar" onClick={() => setJogoIniciado(true)}>Iniciar</button>
+
+                
             </S.DivGame>
 
 
