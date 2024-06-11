cb83e13251a7ca853e574cda63dba473e584793d
leticiaSouzaDev <lec667121@gmail.com>
2024-06-11 15:31:49 -0300

    feat: melhorias no código e estilização da div game

--- src/pages/Game.jsx/Game.jsx
+++ src/pages/Game.jsx/Game.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 
-import * as S from './Style.jsx'
+import * as S from './Style.jsx';
 
 function Game(){
     const [nome, setNome] = useState('');
-    const [jogoIniciado, setJogoIniciado] = useState(false);
-    const [mostrarMensagem, setMostrarMensagem] = useState(true);
+    const [jogoIniciado, setJogoIniciado] = useState(false); 
+    const [mostrarMensagem, setMostrarMensagem] = useState(true); // mostrar a mensagem com o nome do jogador
+    const [letras, setLetras] = useState([]);
+    const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const [mostrarBtnIniciar, setMostrarBtnIniciar] = useState(true);
 
     const handleNomeSubmit = (event) => { // funcão para receber o  nome do jogador
         event.preventDefault();
@@ -16,10 +19,35 @@ function Game(){
         }, 3000)
     }
 
+    // const adicionarLetra = () =>{
+    //     setLetras(alfabeto);
+    //     console.log(letras);
+    // }
+
+
+    const renderizacaoConteudo = () => {
+        if(jogoIniciado){
+            return (
+                <form onSubmit={handleNomeSubmit} className="formularioNome">
+                        <label>Digite seu nome Jogador(a)</label>
+                        <input 
+                            type="text" 
+                            value={nome} 
+                            onChange={(e) => setNome(e.target.value)}>
+                        </input>
+                        <button type="submit" id="btnEnvioNome">enviar</button>
+                    </form>
+            ); 
+        } else if (nome && mostrarMensagem){
+            return <p>Bem - vindo ao jogo {nome}</p>
+        } 
+    }
+
     document.addEventListener('keydown', function(event) { // verificando qual tecla foi precionada
         let teclaPressionada = event.key;
         console.log('Tecla pressionada: ' + teclaPressionada);
     });
+
 
     return(
         <S.SessaoGame>
@@ -27,21 +55,19 @@ function Game(){
             <S.DivGame>
                 <h2>MINI-GAME</h2>
 
-                {jogoIniciado ? (
-                    <form onSubmit={handleNomeSubmit}>
-                        <label>Digite seu nome</label>
-                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}></input>
-                        <button type="submit">enviar</button>
-                    </form>
-                ) : (
-                    nome && mostrarMensagem &&
-                    (<p>Bem - vindo ao jogo {nome}</p>)
+                {renderizacaoConteudo()}
+                {mostrarBtnIniciar && (
+                    <button id="botaoIniciar" onClick={() => {setJogoIniciado(true); setMostrarBtnIniciar(false)}}>Iniciar</button>
+                    
                 )}
-                <button id="botaoIniciar" onClick={() => setJogoIniciado(true)}>Iniciar</button>
 
-                
+                {letras.map((letras, index) =>(
+                    <span key={index}>{letras}</span>
+                ))}
+
             </S.DivGame>
 
+            
 
         </S.SessaoGame>
     )
--- src/pages/Game.jsx/Style.jsx
+++ src/pages/Game.jsx/Style.jsx
@@ -8,12 +8,37 @@ export const SessaoGame = styled.section`
 `
 
 export const DivGame = styled.div`
+    border: 1px solid black;
+
+    padding: 2rem;
+    width: 30%;
+
     display: flex;
     flex-direction: column;
     align-items: center;
 
     #botaoIniciar{
         width: 5rem;
+        color: #ffeb3b;;
+    }
+
+    .formularioNome{
+        padding: 1rem;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .formularioNome > label, input{
+        margin-bottom: 1rem;
+    }
+
+    #btnEnvioNome{
+        padding: 0.5rem;
+        width: 50%;
+        border-radius: 50rem;
+        color: #ffeb3b;;
     }
 
 `
