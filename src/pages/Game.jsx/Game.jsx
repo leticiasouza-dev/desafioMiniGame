16e1e7d6d2a35f2b0beb41d948c739b5b678e6bd
leticiaSouzaDev <lec667121@gmail.com>
2024-06-14 18:13:54 -0300

    feat: adicionando botões de tentar novamente e sair

--- src/pages/Game.jsx/Game.jsx
+++ src/pages/Game.jsx/Game.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 
 import * as S from './Style.jsx';
+import { useNavigate  } from "react-router-dom";
 
 function Game(){
     const [nome, setNome] = useState('');
     const [jogoIniciado, setJogoIniciado] = useState(false); 
-    const [mostrarMensagem, setMostrarMensagem] = useState(true); // mostrar a mensagem com o nome do jogador
     const [letras, setLetras] = useState([]);
     const letrasSorteadas = [];
     const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    
+    //estados para as mensagem e elementos de tela
+    const [mostrarMensagem, setMostrarMensagem] = useState(true); // mostrar a mensagem com o nome do jogador
     const [mostrarBtnIniciar, setMostrarBtnIniciar] = useState(true);
+    const [mostrarBtnTentarNovamente, setMostrarBtnTentarNovamente] = useState(false);
+    const [mostrarBtnSair, setBtnSair] = useState(false)
+
 
     const handleNomeSubmit = (event) => { // funcão para receber o  nome do jogador
         event.preventDefault();
@@ -61,35 +67,49 @@ function Game(){
 
     
 
-let indiceAtual = 0;
-function verificarTecla(teclaPressionada) {
+    let indiceAtual = 0;
+    let mensagemInformacao = '';
+    function verificarTecla(teclaPressionada) {
     
-
-    function teclaCorreta() {
-        console.log("Tecla correta");
-    }
-
-    function teclaIncorreta() {
-        console.log("Tecla incorreta. Você perdeu!");
-    }
-
-    function sequenciaCompleta() {
-        console.log("Sequência completa! Parabéns!");
-    
-        indiceAtual = 0; 
-    }
-
-    if (teclaPressionada === letrasSorteadas[indiceAtual].toLocaleLowerCase()) {
-        teclaCorreta();
-        indiceAtual++;
-        if (indiceAtual === letrasSorteadas.length) {
-            sequenciaCompleta();
+        function teclaCorreta() {
+            console.log("Tecla correta");
         }
-    } else {
-        teclaIncorreta();
-        indiceAtual = 0;
-    }
+
+        function teclaIncorreta () {
+            console.log("Tecla incorreta. Você perdeu!");
+            setMostrarBtnTentarNovamente(true);
+            setBtnSair(true);
+        }
+
+        function sequenciaCompleta() {
+            console.log("Sequência completa! Parabéns!");
+        
+            indiceAtual = 0; 
+
+            // ver a pontuação, jogar novamente
+        }
+
+       
+
+        if (teclaPressionada === letrasSorteadas[indiceAtual].toLocaleLowerCase()) {
+            teclaCorreta();
+            mensagemInformacao = 'Tecla Correta';
+            indiceAtual++;
+            if (indiceAtual === letrasSorteadas.length) {
+                sequenciaCompleta();
+                mensagemInformacao = 'Sequencia incorreta'
+            }
+        } else {
+            mensagemInformacao = 'teclaIncorreta'
+            teclaIncorreta();
+            indiceAtual = 0;
+        } 
 }
+
+    const navigate = useNavigate();
+    const handleSairClick = () => {
+        navigate('/');
+    }
 
 
     return(
@@ -113,6 +133,14 @@ function verificarTecla(teclaPressionada) {
                         ))}
                     </>
                 )}
+
+                {mostrarBtnTentarNovamente && mostrarBtnSair &&
+                    <>
+                    <p>Você errou {mensagemInformacao}</p>
+                    <button>Tentar novamente</button>
+                    <button onClick={handleSairClick}>Sair</button>
+                    </>
+                }
 
             </S.DivGame>
 
